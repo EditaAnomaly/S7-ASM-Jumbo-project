48754aafe3a58ff14e491a8564108565438f2ba4
Jop Huijbers <jophuijbers2001@gmail.com>
2022-10-20 11:54:34 +0200

    feature(barcode-api)
    fetchProduct now performs 2 api requests
    first, it fetches the product from barcode
    second, fetches all extra information from product id

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,29 +1,29 @@
 import 'package:jumbo_app_flutter/models/ingredient.dart';
+import 'package:jumbo_app_flutter/models/price.dart';
 
 class Product {
   final String id;
   final String title;
-  final int price;
+  final Price price;
   final String quantity;
   final String image;
   final List<Ingredient> ingredients;
 
-  Product({
-    required this.id,
-    required this.title,
-    required this.price,
-    required this.quantity,
-    required this.image,
-    required this.ingredients,
-  });
+  Product(
+      {required this.id,
+      required this.title,
+      required this.price,
+      required this.quantity,
+      required this.image,
+      required this.ingredients});
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
       id: json['id'],
       title: json['title'],
-      price: json['prices']['price']['amount'],
+      price: Price.fromJson(json['prices']['price']),
       quantity: json['quantity'],
-      image: json['imageInfo']['primaryView'][2]['url'],
+      image: json['imageInfo']['primaryView'][0]['url'],
       ingredients: List<Ingredient>.from(json['ingredientInfo'][0]
               ['ingredients']
           .map((ingredient) => Ingredient.fromJson(ingredient))),
--- lib/services/api.dart
+++ lib/services/api.dart
@@ -3,22 +3,32 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Api {
-  Future<Product> fetchProduct(productId) async {
-    final url = 'https://mobileapi.jumbo.com/v17/products/$productId';
+  final baseUrl = 'https://mobileapi.jumbo.com/v17/';
+  final headers = {
+    "Accept": "application/json",
+    "User-Agent": "Fontys team green"
+  };
 
-    final headers = {
-      "Accept": "application/json",
-      "User-Agent": "Fontys team green"
-    };
+  Future<Product> fetchProduct(barcode) async {
+    final barcodeResponse = await getRequest('search?q=$barcode');
+    final productId = jsonDecode(barcodeResponse.body)['products']['data'][0]
+        ['id']; // 8718452461158
 
-    final response = await http.get(Uri.parse(url), headers: headers);
-
-    if (response.statusCode != 200) throw Exception('Failed to load product');
-
-    var jsonString =
-        jsonDecode(utf8.decode(response.bodyBytes))['product']['data'];
-    var product = Product.fromJson(jsonString);
+    final productResponse = await getRequest('products/$productId');
+    var productString =
+        jsonDecode(utf8.decode(productResponse.bodyBytes))['product']['data'];
+    var product = Product.fromJson(productString);
 
     return product;
+  }
+
+  Future<http.Response> getRequest(source) async {
+    try {
+      final response =
+          await http.get(Uri.parse(baseUrl + source), headers: headers);
+      return response;
+    } catch (e) {
+      throw Exception(e);
+    }
   }
 }
