06b85b2e2eb4aae49ac6bfa092768234bc96e07b
Edita <102149932+EditaAnomaly@users.noreply.github.com>
2022-11-16 14:53:35 +0100

    alert widget updates
    -moved to separate file
    -removed list scrolling
    -added spacing between allergens

--- lib/pages/basket_page.dart
+++ lib/pages/basket_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:jumbo_app_flutter/models/allergen.dart';
 import 'package:jumbo_app_flutter/models/product.dart';
 import 'package:jumbo_app_flutter/services/product.service.dart';
+import 'package:jumbo_app_flutter/widgets/product_alert.dart';
 
 class BasketPage extends StatefulWidget {
   const BasketPage({super.key});
@@ -24,73 +25,11 @@ class _BasketPageState extends State<BasketPage> {
 
     if (warnings.isEmpty) return;
 
-    _alertAboutProduct(product, warnings);
-  }
-
-  _alertAboutProduct(Product product, List<Allergen> warnings) {
     showDialog(
-      context: context, barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          shape: const Border(
-              left: BorderSide(width: 6.0, color: Color((0xffEEB717)))),
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: const [
-              Image(
-                image: AssetImage('images/warningsmol.png'),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Product Warning',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 20),
-              Text(
-                  'Pay attention! The Product contains the following dangerous ingredients:')
-            ],
-          ),
-          content: SizedBox(
-            width: double.maxFinite,
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: warnings.length,
-              itemBuilder: (context, index) {
-                return Text(warnings[index].message,
-                    style: const TextStyle(
-                        fontSize: 20, fontWeight: FontWeight.bold));
-              },
-            ),
-          ),
-          actions: [
-            TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: const Color(0xffEEB717),
-                textStyle: const TextStyle(fontSize: 20),
-              ),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Add anyway'),
-            ),
-            TextButton(
-                style: OutlinedButton.styleFrom(
-                  side: const BorderSide(
-                    color: Color(0xffEEB717),
-                  ),
-                  textStyle: const TextStyle(fontSize: 20),
-                ),
-                child:
-                    const Text('Cancel', style: TextStyle(color: Colors.black)),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                })
-          ],
-          actionsAlignment: MainAxisAlignment.spaceEvenly,
-        );
-      },
-    );
+        context: context,
+        builder: (BuildContext context) {
+          return ProductAlert(product, warnings);
+        });
   }
 
   @override
