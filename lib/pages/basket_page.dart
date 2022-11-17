482d08859651ffc958c7bd96f0f56bbe08ecebdc
Lukevg <luke.vangenechten@student.fontys.nl>
2022-11-17 15:40:02 +0100

    Created a row for basket items

--- lib/pages/basket_page.dart
+++ lib/pages/basket_page.dart
@@ -47,33 +47,36 @@ class _BasketPageState extends State<BasketPage> {
         foregroundColor: Colors.black,
         child: const Icon(Icons.qr_code_scanner_rounded),
       ),
-      body: Container(
-        margin: const EdgeInsets.all(20),
-        padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
-        height: MediaQuery.of(context).size.height / 30,
-        width: MediaQuery.of(context).size.width / 5,
-        decoration: const BoxDecoration(
-            border: Border(
-              top: BorderSide(
-                  width: 1,
-                  color: Color(0xffEEB717),
-                  style: BorderStyle.solid), //BorderSide
-              bottom: BorderSide(
-                  width: 1,
-                  color: Color(0xffEEB717),
-                  style: BorderStyle.solid), //BorderSide
-              left: BorderSide(
-                  width: 1,
-                  color: Color(0xffEEB717),
-                  style: BorderStyle.solid), //Borderside
-              right: BorderSide(
-                  width: 1,
-                  color: Color(0xffEEB717),
-                  style: BorderStyle.solid), //BorderSide
-            ),
-            borderRadius: BorderRadius.all(Radius.circular(20.0))),
-        child: const Text('amount'),
-      ),
+      body: Row(children: <Widget>[
+        Container(
+          margin: const EdgeInsets.all(20),
+          padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+          height: MediaQuery.of(context).size.height / 30,
+          width: MediaQuery.of(context).size.width / 5,
+          decoration: const BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                    width: 1,
+                    color: Color(0xffEEB717),
+                    style: BorderStyle.solid),
+                bottom: BorderSide(
+                    width: 1,
+                    color: Color(0xffEEB717),
+                    style: BorderStyle.solid),
+                left: BorderSide(
+                    width: 1,
+                    color: Color(0xffEEB717),
+                    style: BorderStyle.solid),
+                right: BorderSide(
+                    width: 1,
+                    color: Color(0xffEEB717),
+                    style: BorderStyle.solid),
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(15.0))),
+          child: const Text('amount'),
+        ),
+        const Text('this is why i choose media...')
+      ]),
     );
   }
 }
