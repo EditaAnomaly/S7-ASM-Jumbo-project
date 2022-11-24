87cc9c92ed2ff6433411f56f0eb85aebd6a96d3d
Duc Hieu Le <duchieule630@gmail.com>
2022-11-24 16:20:29 +0100

    feat: add avatar v1

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -11,34 +11,63 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(children: const <Widget>[
-          Text('Your ',
+        appBar: AppBar(
+          title: Row(children: const <Widget>[
+            Text('Your ',
+                style: TextStyle(
+                  fontSize: 24,
+                )),
+            Text(
+              'Profile',
               style: TextStyle(
                 fontSize: 24,
-              )),
-          Text(
-            'Profile',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w700,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ]),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          centerTitle: false,
+          leadingWidth: 0,
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Avatar(),
+                PreferenceSelector(),
+              ],
+            )));
+  }
+}
+
+// padding: const EdgeInsets.all(20.0),
+class Avatar extends StatelessWidget {
+  const Avatar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(140),
+            ),
+            child: const CircleAvatar(
+              radius: 120,
+              backgroundImage: AssetImage("images/avatar.png"),
             ),
           ),
-        ]),
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        centerTitle: false,
-        leadingWidth: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            PreferenceSelector(),
-          ],
-        ),
-      ),
-    );
+          const Text(
+            'Your name',
+          ),
+        ],
+      )
+    ]);
   }
 }
