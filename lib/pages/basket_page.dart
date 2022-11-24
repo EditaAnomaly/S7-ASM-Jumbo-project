cf51c3be50f71705fa47cead4e588f8e1dd66099
luke-vg <luke.vangenechten@student.fontys.nl>
2022-11-24 15:23:13 +0100

    Basket list view added
    Enabled AddAnyway
    Fixed listview layout
    Add scanned items to list

--- lib/pages/basket_page.dart
+++ lib/pages/basket_page.dart
@@ -15,6 +15,7 @@ class _BasketPageState extends State<BasketPage> {
   final ProductService productService = ProductService();
   late Product product;
   late List<Allergen> warnings;
+  final List<Product> basket = [];
 
   _openBarcodeScanner() async {
     String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
@@ -23,13 +24,27 @@ class _BasketPageState extends State<BasketPage> {
     product = await productService.scan(barcodeScanRes);
     warnings = productService.getWarnings(product);
 
-    if (warnings.isEmpty) return;
+    if (warnings.isEmpty) {
+      _addToBasket(product);
+
+      return;
+    }
 
     showDialog(
         context: context,
         builder: (BuildContext context) {
-          return ProductAlert(product, warnings);
+          return ProductAlert(
+            product,
+            warnings,
+            _addToBasket,
+          );
         });
+  }
+
+  _addToBasket(Product product) {
+    setState(() {
+      basket.add(product);
+    });
   }
 
   @override
@@ -47,35 +62,56 @@ class _BasketPageState extends State<BasketPage> {
         foregroundColor: Colors.black,
         child: const Icon(Icons.qr_code_scanner_rounded),
       ),
-      body: Row(children: <Widget>[
-        Container(
-          margin: const EdgeInsets.all(20),
-          padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
-          height: MediaQuery.of(context).size.height / 30,
-          width: MediaQuery.of(context).size.width / 5,
-          decoration: const BoxDecoration(
-              border: Border(
-                top: BorderSide(
-                    width: 1,
-                    color: Color(0xffEEB717),
-                    style: BorderStyle.solid),
-                bottom: BorderSide(
-                    width: 1,
-                    color: Color(0xffEEB717),
-                    style: BorderStyle.solid),
-                left: BorderSide(
-                    width: 1,
-                    color: Color(0xffEEB717),
-                    style: BorderStyle.solid),
-                right: BorderSide(
-                    width: 1,
-                    color: Color(0xffEEB717),
-                    style: BorderStyle.solid),
+      body: ListView(children: <Widget>[
+        if (basket.isEmpty) ...[
+          const Text('nothing in basket'),
+        ] else ...[
+          for (product in basket)
+            Row(children: <Widget>[
+              Container(
+                  margin: const EdgeInsets.fromLTRB(20, 20, 10, 20),
+                  padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+                  height: MediaQuery.of(context).size.height / 30,
+                  width: MediaQuery.of(context).size.width / 5,
+                  decoration: const BoxDecoration(
+                      border: Border(
+                        top: BorderSide(
+                            width: 1,
+                            color: Color(0xffEEB717),
+                            style: BorderStyle.solid),
+                        bottom: BorderSide(
+                            width: 1,
+                            color: Color(0xffEEB717),
+                            style: BorderStyle.solid),
+                        left: BorderSide(
+                            width: 1,
+                            color: Color(0xffEEB717),
+                            style: BorderStyle.solid),
+                        right: BorderSide(
+                            width: 1,
+                            color: Color(0xffEEB717),
+                            style: BorderStyle.solid),
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                  child: const Center(
+                    child: Text('1'),
+                  )),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                child: Image.network(product.image, height: 30, width: 30),
               ),
-              borderRadius: BorderRadius.all(Radius.circular(15.0))),
-          child: const Text('amount'),
-        ),
-        const Text('this is why i choose media...')
+              Flexible(child: Text(product.title)),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                child: Text(product.price.toString()),
+              ),
+            ]),
+          const Divider(
+            color: Colors.grey,
+            height: 0,
+            thickness: 0.3,
+          )
+        ]
       ]),
     );
   }
