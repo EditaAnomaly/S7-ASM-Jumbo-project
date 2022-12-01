f14db05f557cfbd6550ed425e907d7a236042ba7
Polished Studios <fd.github@outlook.com>
2022-12-01 15:23:31 +0100

    Add barcode scanner to slider & updated alert callbacks with scanner functionality.

--- lib/pages/ar_page.dart
+++ lib/pages/ar_page.dart
@@ -1,11 +1,13 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:jumbo_app_flutter/models/allergen.dart';
 import 'package:jumbo_app_flutter/models/product.dart';
 import 'package:jumbo_app_flutter/services/product.service.dart';
 import 'package:jumbo_app_flutter/widgets/product_alert.dart';
+import 'package:jumbo_app_flutter/widgets/product_alert copy.dart';
+
+import 'package:fast_barcode_scanner/fast_barcode_scanner.dart';
 
 class ArViewPage extends StatefulWidget {
   const ArViewPage({super.key});
@@ -14,43 +16,13 @@ class ArViewPage extends StatefulWidget {
 }
 
 class _ArViewPageState extends State<ArViewPage> {
-  double _panelHeightOpen = 0;
-  final double _panelHeightClosed = 320.0;
-
   final ProductService productService = ProductService();
   late Product product;
   late List<Allergen> warnings;
   final List<Product> basket = [];
-
-  _openBarcodeScanner() async {
-    String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-        "#000000", "Cancel", true, ScanMode.BARCODE);
-
-    product = await productService.scan(barcodeScanRes);
-    warnings = productService.getWarnings(product);
-
-    if (warnings.isEmpty) {
-      _addToBasket(product);
-
-      return;
-    }
-
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return ProductAlert(
-            product,
-            warnings,
-            _addToBasket,
-          );
-        });
-  }
-
-  _addToBasket(Product product) {
-    setState(() {
-      basket.add(product);
-    });
-  }
+  late CameraController cameraController;
+  double _panelHeightOpen = 0;
+  final double _panelHeightClosed = 315.0;
 
   @override
   Widget build(BuildContext context) {
@@ -61,6 +33,7 @@ class _ArViewPageState extends State<ArViewPage> {
         MediaQuery.of(context).padding.bottom -
         5;
     _panelHeightOpen = availableHeight;
+    final bodyHeight = availableHeight - 270;
 
     return Material(
       child: Stack(
@@ -71,7 +44,7 @@ class _ArViewPageState extends State<ArViewPage> {
             maxHeight: _panelHeightOpen,
             minHeight: _panelHeightClosed,
             parallaxEnabled: false,
-            body: _body(),
+            body: _body(bodyHeight),
             panelBuilder: (sc) => _panel(sc),
             borderRadius: const BorderRadius.only(
                 topLeft: Radius.circular(18.0),
@@ -120,11 +93,17 @@ class _ArViewPageState extends State<ArViewPage> {
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text("Shopping basket",
+              children: [
+                const Text("Basket",
                     style: TextStyle(
                       fontWeight: FontWeight.normal,
                       fontSize: 24.0,
+                    )),
+                Text(" (${basket.length.toString()})",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 24.0,
+                      color: Colors.grey,
                     ))
               ],
             ),
@@ -193,12 +172,15 @@ class _ArViewPageState extends State<ArViewPage> {
               ]), // End Basket List
             ),
             const SizedBox(
-              height: 24,
+              height: 50,
             ),
           ],
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: _openBarcodeScanner, // should also close panel
+          // onPressed: _openBarcodeScanner, // should also close panel
+          onPressed: () {
+            print("pressed fab");
+          },
           backgroundColor: const Color(0xffEEB717),
           tooltip: 'Scan',
           foregroundColor: Colors.black,
@@ -235,7 +217,95 @@ class _ArViewPageState extends State<ArViewPage> {
     );
   }
 
-  Widget _body() {
-    return const Center(child: Text("hello :)")); // add AR camera here.
+  _checkBarcode(code, cameraController) async {
+    product = await productService.scan(code);
+    warnings = productService.getWarnings(product);
+
+    if (warnings.isEmpty) {
+      _addProductToBasket(product);
+      Future.delayed(
+          const Duration(milliseconds: 1400), () => _resumeScanning());
+      return;
+    }
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return ProductAlertCopy(
+            product,
+            warnings,
+            _callbackAdd,
+            _callbackCancel,
+          );
+        });
+  }
+
+  _addProductToBasket(Product product) {
+    setState(() {
+      basket.add(product);
+    });
+  }
+
+  _callbackAdd(Product product) {
+    _addProductToBasket(product);
+    _resumeScanning();
+  }
+
+  _callbackCancel() {
+    _resumeScanning();
+  }
+
+  _resumeScanning() {
+    cameraController.resumeDetector();
+  }
+
+  Widget _body(bodyHeight) {
+    cameraController = CameraController.instance;
+
+    return Column(
+      children: [
+        SizedBox(
+          height: bodyHeight,
+          width: double.infinity,
+          child: Expanded(
+            child: BarcodeCamera(
+              types: const [
+                BarcodeType.ean8,
+                BarcodeType.ean13,
+              ],
+              resolution: Resolution.hd720,
+              framerate: Framerate.fps30,
+              mode: DetectionMode.pauseDetection,
+              position: CameraPosition.back,
+              onScan: (code) => {
+                _checkBarcode(code.value, cameraController),
+              },
+              children: const [
+                MaterialPreviewOverlay(
+                    animateDetection: false, aspectRatio: 16.0 / 11.0),
+                // BlurPreviewOverlay(),
+                // Positioned(
+                //   bottom: 35,
+                //   left: 0,
+                //   right: 0,
+                //   child: Column(
+                //     children: const [
+                //       Text("Tooltip"), // Add tooltip for scan feedback?
+                //     ],
+                //   ),
+                // )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
+
+// TODO: 
+// - Merge develop into this branch.
+// - Slider up = disabled camera, slider down = enabled camera.
+// - FAB functionality: slide down basket on press.
+// - (opt) Tooltip after successfully adding item to basket,
+//      and/or animate MaterialPreviewOverlay (https://m2.material.io/design/machine-learning/barcode-scanning.html#usage).
